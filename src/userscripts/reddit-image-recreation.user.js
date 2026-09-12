b01ac47ea7f87e8dc1b497258609981d0f491626
Fynn <fynn@local>
2026-09-13 00:09:40 +0200

    Publish restored 1.28 code as userscript 1.32

--- src/userscripts/reddit-image-recreation.user.js
+++ src/userscripts/reddit-image-recreation.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit Image Recreation
 // @namespace    https://tampermonkey.net/
-// @version      1.28
+// @version      1.32
 // @match        https://www.reddit.com/*
 // @match        https://sh.reddit.com/*
 // @grant        none
